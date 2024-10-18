eecdad80b94e259b85364249adc55d518502d966
Prince <tozprince@gmail.com>
2024-10-19 02:20:22 +0800

    remove unused import

--- programs/rebate_manager/src/state/rebate_info.rs
+++ programs/rebate_manager/src/state/rebate_info.rs
@@ -31,7 +31,7 @@
 * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-use crate::{constants::*, errors::ErrorCode};
+use crate::errors::ErrorCode;
 use anchor_lang::prelude::*;
 
 #[account]
