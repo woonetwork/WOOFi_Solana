25e2ad3b65a39a65fa2e54aaf908772c4c6482f8
Prince <tozprince@gmail.com>
2024-11-04 12:47:13 +0800

    only owner can withdraw

--- programs/woofi/src/instructions/admin/deposit_withdraw.rs
+++ programs/woofi/src/instructions/admin/deposit_withdraw.rs
@@ -5,7 +5,7 @@ use anchor_spl::token::{self, Mint, Token, TokenAccount};
 use crate::{errors::ErrorCode, util::*};
 
 #[derive(Accounts)]
-pub struct DepositWithdraw<'info> {
+pub struct Deposit<'info> {
     pub wooconfig: Box<Account<'info, WooConfig>>,
     pub token_mint: Account<'info, Mint>,
 
@@ -35,7 +35,40 @@ pub struct DepositWithdraw<'info> {
     pub token_program: Program<'info, Token>,
 }
 
-pub fn deposit(ctx: Context<DepositWithdraw>, amount: u128) -> Result<()> {
+#[derive(Accounts)]
+pub struct Withdraw<'info> {
+    #[account(
+        has_one = authority
+    )]
+    pub wooconfig: Box<Account<'info, WooConfig>>,
+    pub token_mint: Account<'info, Mint>,
+
+    pub authority: Signer<'info>,
+
+    #[account(mut,
+        constraint = token_owner_account.owner == authority.key(),
+        constraint = token_owner_account.mint == token_mint.key()
+    )]
+    token_owner_account: Box<Account<'info, TokenAccount>>,
+
+    #[account(mut,
+        has_one = wooconfig,
+        has_one = authority,
+        constraint = woopool.token_mint == token_mint.key()
+    )]
+    pub woopool: Box<Account<'info, WooPool>>,
+
+    #[account(mut,
+        address = woopool.token_vault,
+        constraint = token_vault.mint == token_mint.key()
+      )]
+    pub token_vault: Box<Account<'info, TokenAccount>>,
+
+    #[account(address = token::ID)]
+    pub token_program: Program<'info, Token>,
+}
+
+pub fn deposit(ctx: Context<Deposit>, amount: u128) -> Result<()> {
     let token_owner_account = &ctx.accounts.token_owner_account;
     let token_vault = &ctx.accounts.token_vault;
     let woopool = &mut ctx.accounts.woopool;
@@ -62,7 +95,7 @@ pub fn deposit(ctx: Context<DepositWithdraw>, amount: u128) -> Result<()> {
     Ok(())
 }
 
-pub fn withdraw(ctx: Context<DepositWithdraw>, amount: u128) -> Result<()> {
+pub fn withdraw(ctx: Context<Withdraw>, amount: u128) -> Result<()> {
     let token_owner_account = &ctx.accounts.token_owner_account;
     let token_vault = &ctx.accounts.token_vault;
     let woopool = &mut ctx.accounts.woopool;
